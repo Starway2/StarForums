73e4b31337afa407036acfd3c95cd41e87e9b511
Starway23 <plamentrakiiski2@abv.bg>
2020-10-06 21:33:09 +0300

    Added TinyMCE to comments and post creation, more design updates and fixes

--- Web/StarForums.Web/wwwroot/js/site.js
+++ Web/StarForums.Web/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+tinymce.init({
+    selector: '#text-editor',
+    resize: false,
+    plugins: 'autoresize',
+    max_height: 1000,
+});
