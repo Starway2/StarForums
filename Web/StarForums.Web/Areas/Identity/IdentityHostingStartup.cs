11cb2918e27f3971e13fb5e1f8f5b70693d61143
Starway23 <plamentrakiiski2@abv.bg>
2020-10-14 20:56:37 +0300

    Change register model to include username field. Changed login model to log in with username instead of email

---
+++ Web/StarForums.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(StarForums.Web.Areas.Identity.IdentityHostingStartup))]
+
+namespace StarForums.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
