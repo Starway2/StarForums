9143874530dffadaa11fbaf9fcd456b9138c0b67
Starway23 <plamentrakiiski2@abv.bg>
2020-11-05 22:14:10 +0200

    Dont know what i am doing. Just trying to revert but messed up too bad.

---
+++ Web/StarForums.Web/Controllers/StatusCode.cs
@@ -0,0 +1,22 @@
+﻿namespace StarForums.Web.Controllers
+{
+    using CloudinaryDotNet.Actions;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class StatusCode : Controller
+    {
+        [Route("/Error/Index/{statusCode}")]
+        public IActionResult Index(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    return this.View();
+                default:
+                    break;
+            }
+
+            return this.View();
+        }
+    }
+}
