57ca07da5ad97111a090b5b1f29231da7d9d159f
Starway23 <plamentrakiiski2@abv.bg>
2020-12-05 14:55:46 +0200

    Created view for admins.

--- Web/StarForums.Web/Areas/Administration/Controllers/AdministrationController.cs
+++ Web/StarForums.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -3,11 +3,19 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using StarForums.Common;
-    using StarForums.Web.Controllers;
 
-    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     [Area("Administration")]
     public class AdministrationController : Controller
     {
+        [Authorize]
+        public IActionResult Index()
+        {
+            if (!this.User.IsInRole(GlobalConstants.AdministratorRoleName))
+            {
+                return this.StatusCode(403);
+            }
+
+            return this.View();
+        }
     }
 }
